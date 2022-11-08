1f1cbfd72e9164d074b01912f1cdbe4c23e98beb
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-08 20:30:15 +0100

    Implemented account withdraw and deposit

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -9,12 +9,46 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = ???
-    def deposit (amount: Double): Unit = ???
-    def getBalanceAmount: Double       = ???
+    def withdraw(amount: Double): Either[String, RuntimeException] = {
+        balance synchronized {
+            if (amount <= 0) {
+                return Right(new IllegalAmountException("Amount must be greater than 0"))
+            } else if (amount > getBalanceAmount) {
+                return Right(new NoSufficientFundsException("Insufficient funds"))
+            } else {
+                decreaseBalanceAmount(amount)
+                return Left("Success")
+            }
+        }
+    }
 
-    def transferTo(account: Account, amount: Double) = {
-        bank addTransactionToQueue (this, account, amount)
+    def decreaseBalanceAmount(amount: Double): Unit = {
+        balance synchronized {
+            balance.amount -= amount
+        }
+    }
+
+    def increaseBalanceAmount(amount: Double): Unit = {
+        balance synchronized {
+            balance.amount += amount
+        }
+    }
+
+    def deposit(amount: Double): Either[String, RuntimeException] = {
+        balance synchronized {
+            if (amount <= 0) {
+                return Right(new IllegalAmountException("Amount must be greater than 0"))
+            } else {
+                increaseBalanceAmount(amount)
+                return Left("Success")
+            }
+        }
+    }
+
+    def getBalanceAmount: Double = balance.amount
+
+    def transferTo(account: Account, amount: Double): Unit = {
+        bank addTransactionToQueue(this, account, amount)
     }
 
 
